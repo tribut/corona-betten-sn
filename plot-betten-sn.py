85c46317e8965d675c74409bce1cb48209ec1412
Felix Eckhofer <felix@eckhofer.com>
2021-04-08 11:32:35 +0200

    Reformat source using black

--- plot-betten-sn.py
+++ plot-betten-sn.py
@@ -10,8 +10,12 @@ import json
 import locale
 
 # %% Fetch remote data
-hospital_url = 'https://www.coronavirus.sachsen.de/corona-statistics/rest/hospitalDevelopment.jsp'
-incidence_url = 'https://www.coronavirus.sachsen.de/corona-statistics/rest/incidenceDevelopment.jsp'
+hospital_url = (
+    "https://www.coronavirus.sachsen.de/corona-statistics/rest/hospitalDevelopment.jsp"
+)
+incidence_url = (
+    "https://www.coronavirus.sachsen.de/corona-statistics/rest/incidenceDevelopment.jsp"
+)
 
 hospital_data = json.loads(request.urlopen(hospital_url).read())
 incidence_data = json.loads(request.urlopen(incidence_url).read())
@@ -19,53 +23,71 @@ incidence_data = json.loads(request.urlopen(incidence_url).read())
 # %% Post-processing remote data
 incidence_saxony = None
 for incidence_set in incidence_data:
-    if incidence_set['boundingArea'] == 'Sachsen gesamt':
-        incidence_saxony = incidence_set['incidenceDevelopment']
+    if incidence_set["boundingArea"] == "Sachsen gesamt":
+        incidence_saxony = incidence_set["incidenceDevelopment"]
         break
 
 if incidence_saxony is None:
-    raise SystemExit('Oh noes, saxony is gone...')
+    raise SystemExit("Oh noes, saxony is gone...")
 else:
-    incidence_start = datetime.datetime.fromisoformat(incidence_saxony['startDate'])
+    incidence_start = datetime.datetime.fromisoformat(incidence_saxony["startDate"])
     incidence_x = np.array(
-        [incidence_start + datetime.timedelta(days=x) for x in
-        range(len(incidence_saxony['values']))])
-    incidence_saxony['values'] = np.array(
-        incidence_saxony['values']).astype(np.double)
-    incidence_mask = np.isfinite(incidence_saxony['values'])
+        [
+            incidence_start + datetime.timedelta(days=x)
+            for x in range(len(incidence_saxony["values"]))
+        ]
+    )
+    incidence_saxony["values"] = np.array(incidence_saxony["values"]).astype(np.double)
+    incidence_mask = np.isfinite(incidence_saxony["values"])
 
-hospital_x, occupied_beds = zip(*(hospital_data['numberOfOccupiedBeds']))
-_, occupied_beds_its = zip(*(hospital_data['numberOfOccupiedItsBeds']))
+hospital_x, occupied_beds = zip(*(hospital_data["numberOfOccupiedBeds"]))
+_, occupied_beds_its = zip(*(hospital_data["numberOfOccupiedItsBeds"]))
 
 # %% Create plot
-locale.setlocale(locale.LC_ALL, 'de_DE.UTF_8')
-px = 1 / plt.rcParams['figure.dpi']
-fig, ax_beds = plt.subplots(figsize=(800*px, 450*px))
+locale.setlocale(locale.LC_ALL, "de_DE.UTF_8")
+px = 1 / plt.rcParams["figure.dpi"]
+fig, ax_beds = plt.subplots(figsize=(800 * px, 450 * px))
 ax_incidence = ax_beds.twinx()
 
 # ax_beds.set_xlim(datetime.datetime(2020, 12, 1, 0, 0), incidence_x[-1])
 ax_beds.set_xlim(incidence_x[0], incidence_x[-1])
 
-p1, = ax_beds.plot_date(
+(p1,) = ax_beds.plot_date(
     mdates.epoch2num(np.divide(hospital_x, 1000)),
     np.subtract(occupied_beds, occupied_beds_its),
-    '-', color='tab:brown', label='Belegt auf C19-Normalstation')
-p3, = ax_beds.plot_date(
+    "-",
+    color="tab:brown",
+    label="Belegt auf C19-Normalstation",
+)
+(p3,) = ax_beds.plot_date(
     mdates.epoch2num(np.divide(hospital_x, 1000)),
     occupied_beds,
-    ':', color=p1.get_color(), label='Belegt mit C19 gesamt')
+    ":",
+    color=p1.get_color(),
+    label="Belegt mit C19 gesamt",
+)
 bed_threshold = 1300
-ax_beds.axhline(y=bed_threshold, color='red', linestyle="--")
-ax_beds.text(ax_beds.get_xlim()[0] + 1, bed_threshold * 1.05,
-    'Neue Notbremse', color='red')
-ax_beds.text(0.99, 0.02,
-    'Stand: {}\nDaten von coronavirus.sachsen.de'.format(
-    incidence_x[-1].strftime('%x')), ha='right', transform=ax_beds.transAxes)
+ax_beds.axhline(y=bed_threshold, color="red", linestyle="--")
+ax_beds.text(
+    ax_beds.get_xlim()[0] + 1, bed_threshold * 1.05, "Neue Notbremse", color="red"
+)
+ax_beds.text(
+    0.99,
+    0.02,
+    "Stand: {}\nDaten von coronavirus.sachsen.de".format(
+        incidence_x[-1].strftime("%x")
+    ),
+    ha="right",
+    transform=ax_beds.transAxes,
+)
 
-p2, = ax_incidence.plot_date(
+(p2,) = ax_incidence.plot_date(
     incidence_x[incidence_mask],
-    incidence_saxony['values'][incidence_mask],
-    '-', color='green', label='Inzidenz Sachsen')
+    incidence_saxony["values"][incidence_mask],
+    "-",
+    color="green",
+    label="Inzidenz Sachsen",
+)
 
 ax_beds.set_ylabel("Betten")
 ax_incidence.set_ylabel("7-Tages-Inzidenz")
@@ -77,8 +99,8 @@ ax_beds.yaxis.label.set_color(p1.get_color())
 ax_incidence.yaxis.label.set_color(p2.get_color())
 
 tkw = dict(size=4, width=1.5)
-ax_beds.tick_params(axis='y', colors=p1.get_color(), **tkw)
-ax_incidence.tick_params(axis='y', colors=p2.get_color(), **tkw)
+ax_beds.tick_params(axis="y", colors=p1.get_color(), **tkw)
+ax_incidence.tick_params(axis="y", colors=p2.get_color(), **tkw)
 
 ax_beds.legend(handles=[p1, p3, p2])  # loc='upper left')
 
@@ -91,4 +113,4 @@ ax_beds.xaxis.set_minor_locator(fmt_minor)
 fig.autofmt_xdate()
 
 # %% Save plot
-fig.savefig('inzidenz-betten.pdf')
+fig.savefig("inzidenz-betten.pdf")
